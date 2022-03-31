183300ba0f2bcdee83a9d71293ae73c210fc8ef6
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2022-03-31 08:53:49 +0530

    6th commit

--- java/test1.java
+++ java/test1.java
@@ -1,21 +1,79 @@
 import java.util.Scanner;
 public class test1
-{	
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		for (int i = 1; i <= 5; i++)
-		{
-			for (int j = 1; j <= (5 - i); j++)
-			{
-				System.out.print(" ");
-			}
-			for (int k = i; k >= 1; k--)
-			{
-				System.out.print(k);
-			}
-			System.out.println();
-		}
-	}
+{
+  int[][] arr, tArr;
+  int m, n;
+  /*
+   m = rows
+   n = coulmns
+  */
+  
+  // this method is used to take user input and initialize the arrays...
+  public void in()
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of columns: ");
+    n = sc.nextInt();
+
+    System.out.print("Enter number of rows: ");
+    m = sc.nextInt();
+
+    System.out.println();
+    arr = new int[m][n];
+    tArr = new int[n][m];
+
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        System.out.print("Enter Number: ");
+        arr[i][j] = sc.nextInt();
+      }
+    }
+  }
+  
+  // this method computes and inputs the values in the array, tArr...
+  public void com()
+  {
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        tArr[j][i] = arr[i][j];
+      }
+    }
+  }
+
+  // this method is used for printing the elements of the arrays...
+  public void print()
+  {
+	System.out.println("\nInput: ");
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
+    
+    System.out.println("\nOutput: ");
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        System.out.print(tArr[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String args[])
+  {
+    test1 obj = new test1();
+
+    obj.in();
+    obj.com();
+    obj.print();
+  }
 }
