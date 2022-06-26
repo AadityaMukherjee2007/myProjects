f4fe70aead7ae40f64c1eddd883a566b21635549
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2022-06-26 13:03:07 +0530

    16th commit

---
+++ java/CopyLabel.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.color;
+import java.awt.event.ActionEvent;
+import java.awt.ActionListener;
+
+public class CopyLabel //implements ActionListener
+{
+	public void infoFrame()
+	{
+
+	}
+
+	public void labelFrame()
+	{
+
+	}
+
+	public static void main(String args[])
+	{
+		
+	}
+}
