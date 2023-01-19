73c7f1a08d515da52888bd824c882e68eef29892
AadityaMukherjee2007 <84586322+AadityaMukherjee2007@users.noreply.github.com>
2023-01-19 22:42:41 +0530

    Update mq135_ppm_display.ino

--- arduino/mq135_ppm_display/mq135_ppm_display.ino
+++ arduino/mq135_ppm_display/mq135_ppm_display.ino
@@ -6,6 +6,7 @@ MQ135 s1 = MQ135(A0);
 MQ135 s2 = MQ135(A1);
 
 float ppm1, ppm2, rz1, rz2;
+int modePin = 12;
 
 void displayAirQuality(float ppm1, float ppm2)
 { 
@@ -20,6 +21,14 @@ void displayAirQuality(float ppm1, float ppm2)
   u8g.setPrintPos(5, 57);
   u8g.print(ppm2);
   u8g.print(" ppm");
+
+  Serial.print("Sensor  In: ");
+  Serial.print(ppm1);
+  Serial.println(" ppm");
+  Serial.print("Sensor  Out: ");
+  Serial.print(ppm2);
+  Serial.println(" ppm");
+  Serial.println("\n");
 }
 
 void displayRZeroValues(float rz1, float rz2)
@@ -33,19 +42,23 @@ void displayRZeroValues(float rz1, float rz2)
   u8g.print("Sensor Out:");
   u8g.setPrintPos(5, 57);
   u8g.print(rz2);
+
+  Serial.print("Sensor  In: ");
+  Serial.println(rz1);
+  Serial.print("Sensor  Out: ");
+  Serial.println(rz2);
+  Serial.println("\n");
 }
 
 void setup()
 {
-  pinMode(12, INPUT);
+  pinMode(modePin, INPUT);
   Serial.begin(9600);
 }
 
 void loop()
-{
-  Serial.println(digitalRead(12));
-  
-  if (digitalRead(12))
+{  
+  if (digitalRead(modePin))
   {
     ppm1 = s1.getPPM();
     ppm2 = s2.getPPM();
@@ -67,4 +80,5 @@ void loop()
     }
     while (u8g.nextPage());
   }
+  delay(1000);
 }
