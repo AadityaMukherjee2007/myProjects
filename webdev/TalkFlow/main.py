5057b824defe8b0a72d3b916091a4022f3911cab
Aaditya <aaditya.mukherjee2007@gmail.com>
2023-04-18 15:31:34 +0530

    45th commit

--- webdev/TalkFlow/main.py
+++ webdev/TalkFlow/main.py
@@ -1,16 +1,33 @@
 from flask import Flask, render_template, request
+import sqlite3
+
+con = sqlite3.connect('data.db')
+cur = con.cursor()
+
+cur.execute('CREATE TABLE IF NOT EXISTS info(user TEXT, pass TEXT)')
+con.commit()
 
 app = Flask(__name__)
 
-@app.route('/', methods = ["GET", "POST"])
-def index():
+@app.route('/signup', methods = ["GET", "POST"])
+def signup():
 	if request.method == "POST":
-		username = request.form.get("uname")
-		password = request.form.get("passwd")
+		username = request.form("uname")
+		password = request.form("passwd")
+		userInfo = (username, password)
+		cur.execute('INSERT INTO info VALUES(?, ?)', userInfo)
+		con.commit()
+	return render_template('signup.html')
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+	if request.method == "POST":
+		username = request.form["uname"]
+		password = request.form["passwd"]
 
 		print(username + password)
 		return username + password
-	return render_template('index.html')
+	return render_template('login.html')
 
 
 
