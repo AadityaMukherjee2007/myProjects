f16815be7fc4f491c816aee79312275fcaef162f
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2023-08-02 15:25:24 +0530

    51st commit

--- flutter/test/lib/containers.dart
+++ flutter/test/lib/containers.dart
@@ -14,7 +14,17 @@ class Home extends StatelessWidget
         backgroundColor: Colors.redAccent,
       ),
 
+      body: Container(
+        //padding
+        //padding: const EdgeInsets.all(20),
+        //padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        padding: const EdgeInsets.fromLTRB(10, 20, 30, 40),
 
+        //margin
+        margin: const EdgeInsets.all(30),
+        color: Colors.grey[400],
+        child: const Text('Hello'),
+      ),
     );
   }
 }
