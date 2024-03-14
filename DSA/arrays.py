1d3d5503ff308ed2811567d4cef8d47a9ca86261
AadityaMukherjee2007 <84586322+AadityaMukherjee2007@users.noreply.github.com>
2024-03-14 07:03:28 +0000

    modified:   DSA/arrays.py

--- DSA/arrays.py
+++ DSA/arrays.py
@@ -8,3 +8,14 @@ for n in new_list:
     if n == 1:
         print(True)
         break
+
+numbers = []
+print(len(numbers))
+
+numbers.append(2)
+numbers.append(200)
+
+print(numbers)
+numbers.pop(1)
+numbers.extend([4,5,6])
+print(numbers)
