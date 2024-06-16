147808eccbc98d3ac07f7dac93c9048d7cccf00c
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2024-06-16 09:17:49 +0530

    69th commit

--- arduino/bt_car_control/bt_car_control.ino
+++ arduino/bt_car_control/bt_car_control.ino
@@ -1,4 +1,4 @@
-int m1f = 6, m1b = 7, m2f = 5, m2b = 4;
+int m1f = 7, m1b = 6, m2f = 4, m2b = 5;
 
 int arr[] = {m1f, m1b, m2f, m2b};
 
@@ -14,17 +14,26 @@ void backward()
   digitalWrite(m2b, HIGH);
 }
 
-void right()
+void left()
 {
   digitalWrite(m1f, HIGH);
   digitalWrite(m2f, LOW);
 }
 
-void left()
+void right()
 {
   digitalWrite(m1f, LOW);
   digitalWrite(m2f, HIGH);
 }
+
+void stop()
+{
+  digitalWrite(m1f, LOW);
+  digitalWrite(m2f, LOW);
+  digitalWrite(m1b, LOW);
+  digitalWrite(m2b, LOW);
+}
+
 
 void setup()
 {
@@ -34,15 +43,28 @@ void setup()
     digitalWrite(arr[i], LOW);
   }
   Serial.begin(9600);
+  stop();
 }
 
 void loop()
 {
-  /*while (Serial.available())
+  while (Serial.available())
   {
       char in = Serial.read();
       Serial.println(in);
+
+      switch (in)
+      {
+        case 'F': forward();
+        break;
+        case 'B': backward();
+        break;
+        case 'R': right();
+        break;
+        case 'L': left();
+        break;
+        case 'S': stop();
+        break;
+      }
   }
-  */
-  digitalWrite(m2f, HIGH);
 }
