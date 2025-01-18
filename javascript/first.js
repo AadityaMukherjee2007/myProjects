11cd5c6c10e754f8a5009db510dba15e2d142a91
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2025-01-18 15:38:38 +0530

    75th commit

--- javascript/first.js
+++ javascript/first.js
@@ -5,7 +5,7 @@ x = null;
 y = undefined;
 isFollow = false;
 
-const pi = 3.14
+const pi = 3.14;
 //pi = 3
 
 console.log(full_name);
@@ -16,6 +16,22 @@ console.log(y);
 console.log(isFollow);
 console.log(pi);
 
-console.log(typeof(price))
-console.log(typeof(full_name))
-console.log(typeof(x))
+console.log(typeof(price));
+console.log(typeof(full_name));
+console.log(typeof(x));
+
+const student = {
+    fullName : "Rahul Kumar",
+    age : 20,
+    cgpa : 8.2,
+    isPass : true
+};
+
+console.log(student);
+console.log(typeof(student));
+
+console.log(student["fullName"]);
+console.log(student.fullName);
+
+student["age"] = student["age"] + 1;
+console.log(student["age"]);
---
+++ javascript/practice_q_1.js
@@ -0,0 +1,9 @@
+const product = {
+    productName : "Parker Jotter Standard CT Ball Pen (Black)",
+    price : 270,
+    rating : 4,
+    dealOfTheDay : true,
+    offer : 5
+};
+
+console.log(product);
