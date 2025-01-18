c5d452c444fed895fc00c9a335ae60e6f6c8a77d
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2025-01-18 15:38:38 +0530

    75th commit

---
+++ javascript/practice_q_2.js
@@ -0,0 +1,13 @@
+const profile = {
+    userName: "@shradhakhapra",
+    posts: 195,
+    followers: 569000,
+    following: 4,
+    fullName: "Shradha Khapra",
+    bio: "To educate someone is the bigest privilege❤️",
+    isFollow: false
+};
+
+console.log(profile);
+console.log(typeof(profile));
+console.log(typeof(profile["userName"]));
