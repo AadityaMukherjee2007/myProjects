6bdf919291e4fb986b8a866b8e9ca2cec235f5ac
AadityaMukherjee2007 <aaditya.mukherjee2007@gmail.com>
2025-10-10 20:56:00 +0530

    completed Gram Schmidt Orthogonalisation

--- python/data_science/gramSchmidtOrthogonalisation.py
+++ python/data_science/gramSchmidtOrthogonalisation.py
@@ -23,6 +23,26 @@ def scalarProduct(vector_1: list, vector_2: list) -> Union[float, str]:
     
     return val
 
+# function to add two vectors
+def addVectors(vector1: list, vector2: list) -> list:
+    for i in range(len(vector1)):
+        vector1[i] += vector2[i]
+    return vector1
+
+
+# function to subtract two vectors
+def subtractVectors(vector1: list, vector2: list) -> list:
+    for i in range(len(vector1)):
+        vector1[i] -= vector2[i]
+    return vector1
+
+
+# function to multiply a scalar to a vector
+def multiplyScalar(vector: list, scalar: float) -> list:
+    for i in range(len(vector)):
+        vector[i] *= scalar
+    return vector
+
 
 # function to check whether the set of vectors are orthogonal or not
 def check_orthogonality_of_set(set_of_vectors: list) -> bool:
@@ -34,13 +54,23 @@ def check_orthogonality_of_set(set_of_vectors: list) -> bool:
 
 
 # function to implement Gram Schmidt Orthogonalisation to Orthonormalise a set of orthogonal vectors
-def gram_schmidt(set_of_vectors: list) -> Union[list, str]:
-    output_set = []
-    u1 = set_of_vectors[0]
-    output_set.append(u1)
+def gram_schmidt(v: list) -> Union[list, str]:
 
-    for i in range (1, len(set_of_vectors)):
-        ...
+    # if not check_orthogonality_of_set(v):
+    #     return "set of vectors are not mutually orthogonal"
+
+    u = []
+    u.append(v[0])
+
+    for i in range(1, len(v)):
+        sum = [0 for _ in range(len(v[0]))]
+        for j in range(i):
+            sum = addVectors(sum, multiplyScalar(u[j][:], (scalarProduct(v[i], u[j]) / magnitude(u[j]) ** 2)))
+        u.append(subtractVectors(v[i][:], sum))
+
+    for i in range(len(u)):
+        u[i] = multiplyScalar(u[i], 1 / magnitude(u[i]))
+    return u
     
 
 
@@ -61,32 +91,42 @@ def setInput() -> None:
         print("Cardinality of set cannot be -ve.")
         sys.exit()
     
+    print("\nEnter set of vectors: ")
     for i in range(n):
-        print(f"Vector {i}: ", end="")
         vector = []
         for _ in range(dimension):
-            vector.append(float(input()))
+            vector.append(float(input(f"Vector {i} [{_}]: ")))
         vectors.append(vector)
         print()
 
-    print(vectors)
+    # print(vectors)
 
 
 def main():
-    # setInput()
+    setInput()
+    print(gram_schmidt(vectors))
 
-    # vector_1 = [1, 2, 4]
-    # vector_2 = [2, -1]
-    # print(scalarProduct(vector_1, vector_2))
+    # test_vectors = [
+    #     [3, 0, 0],
+    #     [0, -2, 0],
+    #     [0, 0, 5]
+    # ]
 
-#     vectors = [
-#     [1, 2, -1],
-#     [2, -1, 0],
-#     [1, 1, 2]
-# ]   
-#     print(check_orthogonality_of_set(vectors))
+    # print(magnitude([3, 0, 0]), magnitude([0, -2, 0]))
+    # print(scalarProduct([3, 0, 0], [0, -2, 0]))
 
-    ...
+    # test_vectors = [
+    #     [1, 1, 0],
+    #     [1, 0, 1],
+    #     [0, 1, 1]
+    # ]
+
+    # print(gram_schmidt(test_vectors)) 
+
+    # v1 = [1, 2, 3]
+    # v2 = [3, 2, 1]
+    # print(subtractVectors(v1, v2))
+
 
 
 if __name__ == "__main__":
